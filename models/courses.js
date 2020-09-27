8dae5cc2c80fedd34585f00d2f514f9c92821e04
DoHuynhDuc <dohuynhduc8316@gmail.com>
2020-09-27 16:33:54 +0700

    schema model course

---
+++ models/courses.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const CourseSchema = new mongoose.Schema({
+    _id: { type: String},
+    name : { type: String },
+    author: { type: String },
+    tags: { type: [String] },
+    date: { type: Date, default: Date.now },
+    isPusblished: { type:Boolean },
+    price: { type: Number }
+})
+
+const Course = mongoose.model('Course', CourseSchema, 'Course');
+module.exports = Course;
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const mongoose = require('mongoose');
+const Course = require('./models/courses');
 
 mongoose.connect('mongodb://localhost:27017/test-mongoose', {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
     .then(() => console.log('kết nối thành công'))
+    .catch(err => console.log(err))
+
+const newCourse = new Course({
+    _id: "abc123",
+    name: "nodejs",
+    author: "tg1",
+    tags: ["tag1", "tag2"],
+    isPusblished: true,
+    price: 10
+});
+
+newCourse.save()
+    .then(course => console.log(course))
     .catch(err => console.log(err))
